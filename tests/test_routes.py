360fff7de7dd7471568ab6efbf663639dc668655
vic-aguilar <ing.victor.aguilar@gmail.com>
2023-07-28 06:51:43 -0400

    added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -190,5 +190,11 @@ class TestAccountService(TestCase):
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
 
+    def test_xorigin_resource_sharing(self):
+        """Test cross-origin resource sharing"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+
 
     
